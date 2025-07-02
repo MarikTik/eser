055bc0d7351c64396e1f276fc73bb0a92029be06
Mark <mtik.philosopher@gmail.com>
2025-07-02 14:09:57 -0700

    added tools module for all tools inclusion and defined ser_tools doxygen group

---
+++ ser/tools/tools.hpp
@@ -0,0 +1,18 @@
+/**
+* @file tools.hpp
+* @brief Aggregator header for all utilities in `ser/tools/`.
+*
+* @defgroup ser_tools Serialization Tools
+* @brief Utility components for the `ser` serialization library.
+*
+* The `ser_tools` group includes:
+* - Fallbacks like `byte.hpp` for missing standard library types
+* - Type traits for metaprogramming
+* - Miscellaneous utilities supporting serialization and embedded development
+*/
+#ifndef SER_TOOLS_TOOLS_HPP_
+#define SER_TOOLS_TOOLS_HPP_
+#include "byte.hpp"
+#include "utils.hpp"
+#include "traits.hpp"
+#endif // SER_TOOLS_TOOLS_HPP_
